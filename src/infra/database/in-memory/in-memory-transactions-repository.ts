fca396a3c68a26e6b7ff26b5f9dcfd367c36fd60
Guilherme <guilhermehenrique12308@gmail.com>
2024-10-07 21:45:34 -0300

    feat: add in memory transaction repository

---
+++ src/infra/database/in-memory/in-memory-transactions-repository.ts
@@ -0,0 +1,73 @@
+import { TransactionsRepository } from "@/application/protocols/database"
+import { Transaction } from "@/domain/entities"
+
+export class InMemoryTransactionsRepository implements TransactionsRepository {
+  private database: Transaction[] = []
+  
+  async create(input: TransactionsRepository.Create.Input): TransactionsRepository.Create.Output {
+    const transaction: Transaction = {
+      transactionId: input.transactionId ?? "transactionId_01",
+      userId: input.userId,
+      title: input.title,
+      category: input.category,
+      type: input.type,
+      value: input.value,
+      createdAt: input.createdAt,
+      updatedAt: new Date()
+    }
+
+    this.database.push(transaction)
+
+    return transaction
+  }
+
+  async update(input: TransactionsRepository.Update.Input): TransactionsRepository.Update.Output {
+    const transactionIndex = this.database
+      .findIndex(item => item.transactionId === input.transactionId)
+
+    const transaction = this.database[transactionIndex]
+
+    const updatedTransaction: Transaction  = {
+      transactionId: transaction.transactionId,
+      userId: transaction.userId,
+      title: input.title,
+      type: input.type,
+      category: input.category,
+      value: input.value,
+      createdAt: input.createdAt,
+      updatedAt: transaction.updatedAt,
+    }
+
+    this.database[transactionIndex] = updatedTransaction
+    return updatedTransaction
+  }
+
+  async delete(input: TransactionsRepository.Delete.Input): TransactionsRepository.Delete.Output {
+    const transactions = this.database.
+      filter(item => item.transactionId !== input.transactionId)
+
+    this.database = transactions
+  }
+
+  async fetch(input: TransactionsRepository.Fetch.Input): TransactionsRepository.Fetch.Output {
+    let transactions = this.database.filter(item => item.userId === input.userId)
+
+    if(input.query) {
+      transactions = this.database
+        .filter(item => item.userId === input.userId && item.type === input.query)
+    }
+
+    return transactions
+  }
+
+  async findById(input: TransactionsRepository.FindById.Input): TransactionsRepository.FindById.Output {
+    const transaction = this.database
+      .find(item => item.transactionId === input.transactionId)
+
+    if(!transaction) {
+      return null
+    }
+
+    return transaction
+  }
+}
--- src/infra/database/in-memory/in-memory-users-repository.ts
+++ src/infra/database/in-memory/in-memory-users-repository.ts
@@ -3,40 +3,40 @@ import { UsersRepository } from "@/application/protocols/database"
 import { User } from "@/domain/entities"
 
 export class InMemoryUsersRepository implements UsersRepository {
-    private database: User[] = []
+  private database: User[] = []
 
-    async create(input: UsersRepository.Create.Input): UsersRepository.Create.Output {
-      const user: User = {
-        userId: input.userId ?? "userId-01",
-        name: input.name,
-        email: input.email,
-        password: input.password,
-        transactions: [],
-        createdAt: new Date()
-      }
-
-      this.database.push(user)
-
-      return user
+  async create(input: UsersRepository.Create.Input): UsersRepository.Create.Output {
+    const user: User = {
+      userId: input.userId ?? "userId-01",
+      name: input.name,
+      email: input.email,
+      password: input.password,
+      transactions: [],
+      createdAt: new Date()
     }
 
-    async findById(input: UsersRepository.FindById.Input): UsersRepository.FindById.Output {
-      const user = this.database.find((item) => item.userId === input.userId)
-  
-      if (!user) {
-        return null
-      }
-  
-      return user
+    this.database.push(user)
+
+    return user
+  }
+
+  async findById(input: UsersRepository.FindById.Input): UsersRepository.FindById.Output {
+    const user = this.database.find((item) => item.userId === input.userId)
+
+    if (!user) {
+      return null
     }
 
-    async findByEmail(input: UsersRepository.FindByEmail.Input): UsersRepository.FindByEmail.Output {
-      const user = this.database.find((item) => item.email === input.email)
+    return user
+  }
 
-      if (!user) {
-        return null
-      }
-  
-      return user
+  async findByEmail(input: UsersRepository.FindByEmail.Input): UsersRepository.FindByEmail.Output {
+    const user = this.database.find((item) => item.email === input.email)
+
+    if (!user) {
+      return null
     }
+
+    return user
+  }
 }
