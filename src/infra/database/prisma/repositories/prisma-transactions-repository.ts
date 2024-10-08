cad7b51fc3f83245fc8ab0054da724439aec0b7d
Guilherme <guilhermehenrique12308@gmail.com>
2024-10-08 20:10:04 -0300

    feat: add prisma transactions repository

---
+++ src/infra/database/prisma/repositories/prisma-transactions-repository.ts
@@ -0,0 +1,66 @@
+import { TransactionsRepository } from "@/application/protocols/database"
+import { prisma } from ".."
+
+export class PrismaTransactionsRepository implements TransactionsRepository {
+  async create(input: TransactionsRepository.Create.Input): TransactionsRepository.Create.Output {
+    const transaction = await prisma.transaction.create({
+      data: {
+        userId: input.userId,
+        title: input.title,
+        type: input.type,
+        category: input.category,
+        value: input.value,
+        createdAt: input.createdAt,
+      }
+    })
+
+    return transaction
+  }
+
+  async update(input: TransactionsRepository.Update.Input): TransactionsRepository.Update.Output {
+    const transaction = await prisma.transaction.update({
+      where: {
+        transactionId: input.transactionId
+      },
+      data: {
+        userId: input.userId,
+        title: input.title,
+        type: input.type,
+        category: input.category,
+        value: input.value,
+        createdAt: input.createdAt,
+      }
+    })
+
+    return transaction
+  }
+
+  async delete(input: TransactionsRepository.Delete.Input): TransactionsRepository.Delete.Output {
+    await prisma.transaction.delete({
+      where: {
+        transactionId: input.transactionId
+      }
+    })    
+  }
+
+  async fetch(input: TransactionsRepository.Fetch.Input): TransactionsRepository.Fetch.Output {
+    const transactions = await prisma.transaction.findMany({
+      where: {
+        userId: input.userId,
+        type: input.query
+      }
+    })
+
+    return transactions
+  }
+
+  async findById(input: TransactionsRepository.FindById.Input): TransactionsRepository.FindById.Output {
+    const transaction = await prisma.transaction.findFirst({
+      where: {
+        transactionId: input.transactionId
+      }
+    })
+
+    return transaction
+  }
+}
