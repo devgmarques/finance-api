3c39d592420331bebd8012b5a6d67c2ff2aad148
Guilherme <guilhermehenrique12308@gmail.com>
2024-10-08 20:13:48 -0300

    feat: add bcrypt hash repository

---
+++ src/infra/crypto/bcrypt/bcrypt-hashs-repository.ts
@@ -0,0 +1,16 @@
+import { HashRepository } from "@/application/protocols/crypto"
+import { compare, hash } from "bcryptjs"
+
+export class BcryptHashRepository implements HashRepository {
+  async create(input: HashRepository.Create.Input): HashRepository.Create.Output {
+    const stringHashed = await hash(input.string, input.salt)
+
+    return stringHashed
+  }
+
+  async compare(input: HashRepository.Compare.Input): HashRepository.Compare.Output {
+    const hashCompare = await compare(input.string, input.hash)
+
+    return hashCompare
+  }
+}
