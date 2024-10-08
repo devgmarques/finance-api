ae289946d6cfec2390446abf27e02b28cac6fc25
Guilherme <guilhermehenrique12308@gmail.com>
2024-10-07 21:44:29 -0300

    feat: add transactions repository type

---
+++ src/application/protocols/database/transactions-repository.ts
@@ -0,0 +1,64 @@
+import { Transaction } from "@/domain/entities"
+
+export type TransactionsRepository = {
+  create(input: TransactionsRepository.Create.Input): TransactionsRepository.Create.Output
+  findById(input: TransactionsRepository.FindById.Input): TransactionsRepository.FindById.Output
+  update(input: TransactionsRepository.Update.Input): TransactionsRepository.Update.Output
+  delete(input: TransactionsRepository.Delete.Input): TransactionsRepository.Delete.Output
+  fetch(input: TransactionsRepository.Fetch.Input): TransactionsRepository.Fetch.Output
+}
+
+export namespace TransactionsRepository {
+  export namespace Create {
+    export type Input = {
+      transactionId?: string
+      userId: string
+      title: string
+      value: number
+      type: "income" | "expense"  
+      category: string 
+      createdAt: Date 
+    }
+
+    export type Output = Promise<Transaction>
+  }
+
+  export namespace Update {
+    export type Input = {
+      transactionId: string
+      userId: string
+      title: string
+      value: number
+      type: "income" | "expense"  
+      category: string 
+      createdAt: Date 
+    }
+
+    export type Output = Promise<Transaction>
+  }
+
+  export namespace Delete {
+    export type Input = {
+      transactionId: string
+    }
+
+    export type Output = Promise<void>
+  }
+
+  export namespace FindById {
+    export type Input = {
+      transactionId: string
+    }
+
+    export type Output = Promise<Transaction | null>
+  }
+
+  export namespace Fetch {
+    export type Input = {
+      userId: string
+      query?: "income" | "expense"  
+    }
+
+    export type Output = Promise<Transaction[]>
+  }
+}
